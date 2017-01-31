8159ab91b66ee0b9d67365170fe40ae478e4ac59
Adrian Kujawski <adrian.kujawski@outlook.com>
2017-01-31 16:02:23 +0100

    Update

---
+++ Dovecote/Provider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dovecote {
+	class Provider {
+
+		HodowlaEntities _entity;
+		HodowlaEntities Entity {
+			get {
+				if (_entity != null) return _entity;
+
+				_entity = new HodowlaEntities();
+				return _entity;
+			}
+		}
+
+		static void Do<T>(DbSet<T>) {
+			
+		}
+	}
+}
